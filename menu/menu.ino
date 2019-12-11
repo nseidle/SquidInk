001ffe647c9f745d99485677371f383200bb8f06
Lili Cushing-Quevli <licu3566@colorado.edu>
2019-12-11 11:15:40 -0700

    finished menu
    Now has EEPROM features, retrieves wincode and stores it in EEPROM.
    Prints code to serial monitor, if desired, but can also run unattended and on a wall wart, not plugged into a computer

--- menu/menu.ino
+++ menu/menu.ino
@@ -1,6 +1,6 @@
 
 //created by Lili Cushing-Quevli
-//modified: December 1st, 2019
+//modified: December 11th, 2019
 
 //IN PROGRESS
 // 1) Match with relay switches
@@ -9,15 +9,10 @@
 // 4) Edit button pushing code to work with larger than length 3 (optional)
 // 5) TEST THIS CODE TO MAKE SURE IT FUNCTIONS AS PLANNED
 
+//this is testing
 
-#include <Servo.h>
-Servo myservo;
-//citation for Servo library and code:
-//by BARRAGAN <http://barraganstudio.com>
-//This example code is in the public domain.
-//https://www.arduino.cc/en/Tutorial/Sweep
-// modified 8 Nov 2013
-// by Scott Fitzgerald
+#include <EEPROM.h>
+
 //****RELAYS*****
 int relay1 = 13;
 int relay2 = 2;
@@ -30,7 +25,7 @@ int relay8 = 8;
 int relay9 = 9;
 int relay0 = 10;
 int EnterA = 11;
-
+bool resumeLastKnown = 0;
 int readResistor;
 
 //*******LEDS**************
@@ -39,6 +34,11 @@ int LED2 = A4;
 int LED3 = A3;
 int LED4 = A2;
 int LED5 = A1;
+
+
+//**********EEPROM***********
+int addr = 0;
+
 
 int photoResistor = A0;
 
@@ -50,13 +50,15 @@ int timeOut = 1;
 //used in the menu, typesetting
 float reading = 0;
 //array of all of manually entered values
-float testedComboArray[100];
+float testedComboArray[2];
+int randSelectCombo[1000];
 //keep track of which value in the array is free :)
 int increment = 0;
 //entered code, set with returned value. If it's -1, something's wrong
 int codeLength = 0;
 //used in loopSequence function
 int comboArray[3] = {0, 0, 0};
+int winCodeArray[3] = {0, 0, 0};
 
 //*****SERVO MOTOR********
 //position of motor
@@ -90,8 +92,14 @@ void setup()
   digitalWrite(relay0, HIGH);
   digitalWrite(EnterA, HIGH);
 
+  //fill the random combo array with values from 0-999
+  int beginningIncrement = 0;
+  for (int i = 0; i < 1000; i++)
+  {
+    randSelectCombo[i] = i;
+  }
 
-  myservo.attach(servoPin);
+
   Serial.begin(9600);
 
   printMenu();
@@ -101,7 +109,7 @@ void setup()
 
 void loop() {
 
-  readResistor = analogRead(photoResistor);
+
   while (Serial.available() > 0)
   {
 
@@ -115,10 +123,11 @@ void loop() {
     }
 
     //Serial.println(reading);
+
     if (reading == '1')
     {
+      startCombo();
 
-      codeLength = enterLength();
 
     }
 
@@ -136,10 +145,18 @@ void loop() {
 
     else if (reading == '4')
     {
-      resetCombo();
+      startCombo();
 
 
     }
+
+    else if (reading == '5')
+    {
+
+      retrieveWinCode();
+
+    }
+
 
     else
     {
@@ -159,43 +176,15 @@ void printMenu()
 {
   Serial.println("Cracking a Keypad Safe, Choose an option");
   Serial.println("******When the safe combo is found, the squids will light up*******");
-  Serial.println("1. Enter how long the combination is");
+  Serial.println("1. Set starting code");
   Serial.println("2. Manually enter a code");
   Serial.println("3. Start loop sequence");
   Serial.println("4. Set Code Manually");
+  Serial.println("5. Retrieve Win Code :)");
 
 
 }
 
-int enterLength()
-{
-  delay(10);
-  while (Serial.available() > 0) {
-
-    Serial.read();
-    //clears out the buffer
-
-  }
-
-  Serial.println("ENTER THE LENGTH OF COMBO");
-  while (Serial.available() == 0) {
-
-    //Do nothing
-  }
-  while (Serial.available() > 0)
-  {
-
-    //if there's something to read, take that number
-    //make it the length
-
-    int inputLength = Serial.parseInt();
-    Serial.print(inputLength);
-
-    return inputLength;
-
-  }
-
-}
 
 
 //TO DO: Modify so that the entered code is saved and skips over it
@@ -243,10 +232,10 @@ void manualEnter()
       unsigned secondVal = (n / 10U) % 10;
       unsigned thirdVal = (n / 1U) % 10;
 
-      Serial.println(firstVal);
-      Serial.println(secondVal);
-      Serial.println(thirdVal);
-
+      //      Serial.println(firstVal);
+      //      Serial.println(secondVal);
+      //      Serial.println(thirdVal);
+      //
 
       //press the numbers entered
       pressNumber(firstVal);
@@ -289,111 +278,241 @@ void loopSequence()
   Serial.println("Combo array is:");
 
   bool found = 0;
-  if (codeLength != 0)
+
+
+  int addrTest = 0;
+  int comboVal;
+  if (EEPROM.read(7) == 1)
   {
-    while (found == 0)
+    Serial.println("There was something previously stored here...");
+    for (int j = 0; j < 3; j++)
     {
-      if (reading == 'f')
+      comboVal = EEPROM.read(addrTest);
+      addrTest += 2;
+
+      Serial.print(comboVal, DEC);
+
+
+      comboArray[j] = comboVal;
+
+    }
+  }
+  Serial.print("\n");
+
+  while (found == 0)
+  {
+
+    //by default, array is set to have all zeros
+    //start with 0,0,0 and then increment the last value, going until the last value hits the tenth value
+    //increment the second value and start the for loop again
+
+
+
+    for (int i = comboArray[2]; i < 10; i++)
+    {
+      //call switch case menu to execute the pressing of the buttons
+
+
+      pressNumber(comboArray[0]);
+
+      pressNumber(comboArray[1]);
+
+      pressNumber(i);
+
+
+
+      pressEnter();
+      Serial.print(comboArray[0]);
+      Serial.print(",");
+      Serial.print(comboArray[1]);
+      Serial.print(",");
+      Serial.println(i);
+
+      addrTest = 0;
+
+      for (int j = 0; j < 3; j++)
       {
-        Serial.println("You have exited the combos sequence, please press another option on the menu to proceed");
-        return;
-      }
-      //by default, array is set to have all zeros
-      //start with 0,0,0 and then increment the last value, going until the last value hits the tenth value
-      //increment the second value and start the for loop again
-      for (int i = 0; i < 10; i++)
-      {
-        //call switch case menu to execute the pressing of the buttons
-
-        pressNumber(comboArray[0]);
-
-
-
-        pressNumber(comboArray[1]);
-
-
-        pressNumber(i);
-
-
-        pressEnter();
-        delay(1000);
-        if (comboArray[0] == 1 && comboArray[1] == 0 && i == 0)
-        {
-          Serial.print("100 has been REACHED!!!!!");
-        }
-
-        timeOut++;
-
-        if (winState())
-        {
-
-          Serial.println("Congratulations, The correct combo has been found! Your Combo is: ");
-          Serial.print(comboArray[0]);
-          Serial.print(", ");
-          Serial.print(comboArray[1]);
-          Serial.print(", ");
-          Serial.print(i);
-          turnHandle();
-          Serial.println("Exiting function....");
-          return;
-        }
-
-
-
-        if (timeOut == 3)
-        {
-          delay(30000);
-        }
-
-        else
-        {
-          stopSequence();
-
-        }
-
-      }
-      //if the second combo does not have the value nine in it
-      if (comboArray[1] < 9)
-      {
-        //move up one.
-        //Example : instead of 0,0,0
-        // it is now 0,1,[i]
-        comboArray[1] += 1;
-
+        int writeCombo = 0;
+        writeCombo = comboArray[j];
+        EEPROM.write(addrTest, writeCombo);
+        addrTest += 2;
 
       }
 
-      else
+
+
+      if (winState())
       {
-        //if we have already gone through the second and third values with the current first value
-        //and none worked, we increment the first value and try every possible value for the
-        //second and third
-        if (comboArray[1] >= 9)
-        {
-          comboArray[0] += 1;
-          comboArray[1] = 0;
-        }
+        Serial.println("WINNING CODE WAS:");
 
-        //        else
-        //        {
-        //          //all other codes have been exhausted, if the code has not been found, exit the while loop
-        //          found = 1;
-        //
-        //
-        //        }
+        Serial.print(comboArray[0]);
+        Serial.print(",");
+        Serial.print(comboArray[1]);
+        Serial.print(",");
+        Serial.println(i);
+        int tracker = 8;
+
+        //save in the secondary values of EEPROM, get in other function
+        EEPROM.write(tracker, comboArray[0]);
+        tracker += 2;
+        EEPROM.write(tracker, comboArray[1]);
+        tracker += 2;
+        EEPROM.write(tracker, i);
 
 
+        //        Serial.println("Congratulations, The correct combo has been found! Your Combo is: ");
+        //        Serial.print(comboArray[0]);
+        //        Serial.print(", ");
+        //        Serial.print(comboArray[1]);
+        //        Serial.print(", ");
+        //        Serial.print(i);
+        //        //turnHandle();
+        //        Serial.println("Exiting function....");
+        found = 1;
+        break;
       }
+
+
+
+      if (timeOut % 3 == 0)
+      {
+        stopSequence();
+      }
+      timeOut++;
 
 
     }
 
+    comboArray[2] = 0;
+    //if the second combo does not have the value nine in it
+    if (comboArray[1] < 9)
+    {
+      //move up one.
+      //Example : instead of 0,0,0
+      // it is now 0,1,[i]
+      comboArray[1] += 1;
+
+
+    }
+
+    else
+    {
+      //if we have already gone through the second and third values with the current first value
+      //and none worked, we increment the first value and try every possible value for the
+      //second and third
+      if (comboArray[1] >= 9)
+      {
+        comboArray[0] += 1;
+        comboArray[1] = 0;
+      }
+
+
+
+    }
+
+
   }
-  else
+  return;
+
+}
+
+void startCombo()
+{
+  delay(10);
+  Serial.println("ENTER THE COMBO TO BEGIN LOOP (Do not use letters, please only 3 digits long)");
+  while (Serial.available() > 0)
   {
-    Serial.println("Does not have code length. Please enter before proceeding");
-    return;
+
+    //if there's something to read, take that number
+    //save it in a value/ array
+    //for now has an array of a large size, but could consider adding vector library, at the cost of memory
+
+    //wait for carriage return
+
+    while (Serial.available() > 0) {
+
+      Serial.read();
+      //clears out the buffer
+
+    }
+
+
+    while (Serial.available() == 0) {
+
+      //Do nothing
+    }
+    while (Serial.available() > 0)
+    {
+
+      //if there's something to read, take that number
+      //make it the length
+
+      int testCombo = Serial.parseInt();
+      Serial.print("The combo you've entered is: ");
+      Serial.println(testCombo);
+
+      //code citation
+      //by Christop
+      //source link: https://forum.arduino.cc/index.php?topic=307479.0
+      //parses the full code entered and separates it into individual numbers
+      unsigned n = testCombo;
+      unsigned firstVal = (n / 100U) % 10;
+      unsigned secondVal = (n / 10U) % 10;
+      unsigned thirdVal = (n / 1U) % 10;
+
+      Serial.println("Setting the code....");
+      int counting = 0;
+
+      EEPROM.write(counting, firstVal );
+      counting += 2;
+      EEPROM.write(counting, secondVal);
+      counting += 2;
+      EEPROM.write(counting, thirdVal);
+
+      EEPROM.write(7, 1);
+
+      return;
+    }
   }
+
+
+
+}
+
+void retrieveWinCode()
+{
+  Serial.println("Entered retrieve Win");
+
+  int tracker = 8;
+  int saver = 0;
+  
+  Serial.print("The Safes code is: ");
+  saver = EEPROM.read(tracker);
+//  Serial.println(saver);
+//  Serial.print(saver);
+
+  winCodeArray[0] = saver;
+  
+  
+  tracker += 2;
+
+  saver = EEPROM.read(tracker);
+//  Serial.print(saver);
+  winCodeArray[1] = saver;
+  tracker += 2;
+
+  saver = EEPROM.read(tracker);
+//  Serial.print(saver);
+  winCodeArray[2] = saver;
+
+  Serial.print("YOUR WNNING CODE WAS: ");
+  for (int i = 0; i < 3; i++)
+  {
+   
+    Serial.print(winCodeArray[i]);
+
+  }
+  Serial.print("\n");
 
 
 
@@ -511,6 +630,8 @@ void pressNumber( int var)
 
 }
 
+
+
 bool winState()
 {
   int counter = 0;
@@ -538,22 +659,21 @@ bool winState()
 
 
 
-  Serial.println("CODE FOUND!!!!!!");
-
-  while (1) 
-  {
-    digitalWrite(LED1, HIGH);
-    delay(500);
-    digitalWrite(LED2, HIGH);
-    delay(500);
-    digitalWrite(LED3, HIGH);
-    delay(500);
-    digitalWrite(LED4, HIGH);
-    delay(500);
-    digitalWrite(LED5, HIGH);
+  //Serial.println("CODE FOUND!!!!!!");
 
 
-  }
+  digitalWrite(LED1, HIGH);
+  delay(500);
+  digitalWrite(LED2, HIGH);
+  delay(500);
+  digitalWrite(LED3, HIGH);
+  delay(500);
+  digitalWrite(LED4, HIGH);
+  delay(500);
+  digitalWrite(LED5, HIGH);
+
+
+
 
 
 
@@ -564,25 +684,13 @@ bool winState()
 }
 
 
-void turnHandle()
-{
 
-  //I DID NOT WRITE THIS CODE
-  //reference: https://www.arduino.cc/en/Tutorial/Sweep
-
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-
-}
 
 
 void stopSequence()
 {
-  //delay for a four minutes and 30 seconds while the pcb times out
-  delay(270000);
+  //delay for a four minutes and 46 seconds while the pcb times out
+  delay(286500);
 
 }
 
@@ -681,7 +789,7 @@ void pressEnter()
   digitalWrite(EnterA, LOW);
   delay(delayTime);
   digitalWrite(EnterA, HIGH);
-  delay(delayTime);
+  delay(10);
 
 
 }
